2105685286f62efd77ff75994d0e95ab5b4e676f
I hate iu <101011776+finetglazer@users.noreply.github.com>
2023-01-02 22:18:32 +0700

    learn data and var

---
+++ java_learning/data_type_and_var/var_var.java
@@ -0,0 +1,19 @@
+
+class var_var
+{
+  public static void main(String[] args)
+            {
+                int a=100;
+                int b=12;
+                int c=a+b;
+                String s="hello bro";
+                System.out.println(a);
+                System.out.println(c);
+                System.out.println(s);
+                String k=s+"as";
+                System.out.println(k);
+            }
+        
+}
+          
+    
